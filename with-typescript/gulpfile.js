e7c3749eaf69e6b85320a590e472dd285aeaa868
Duncan Mak <duncan@a-chinaman.com>
2015-03-14 20:13:14 -0400

    Try to use tsify

--- with-typescript/gulpfile.js
+++ with-typescript/gulpfile.js
@@ -3,49 +3,32 @@ var del        = require('del');
 var gulp       = require('gulp');
 var serve      = require('gulp-serve');
 var source     = require('vinyl-source-stream');
-var ts         = require('gulp-typescript');
 
 var htmlSources = [
-    'index.html'
+    './index.html'
 ];
 
 var typescriptSources = [
+    './src/hello.ts',
     './src/main.ts'
 ];
-
-var jsSources = typescriptSources.map(function (i) {
-    return i
-        .replace('src/', 'js/')
-        .replace('.ts', '.js');
-});
 
 var typings = [
     './typings/react/react.d.ts',
     './typings/underscore/underscore.d.ts'
 ];
 
-var tsProject = ts.createProject({
-    noImplicitAny: true,
-    module: 'commonjs'
-});
-
-gulp.task('build', function() {
-    return gulp.src(typescriptSources.concat(typings))
-        .pipe(ts(tsProject)).js
-        .pipe(gulp.dest('js'));
-});
-
 gulp.task('browserify', function () {
-    browserify({ entries: jsSources, paths: ['./js'] })
-	.bundle()
-	.on('error', function(err){ console.log (err.message); this.end(); })
-	.pipe(source('bundle.js'))
-	.pipe(gulp.dest('dist'));
+    browserify({ entries: typescriptSources.concat(typings), path: ['./src'], debug: true })
+        .plugin('tsify')
+        .bundle()
+        .on('error', function(err){ console.log (err.message); this.end(); })
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('watch', function() {
-    gulp.watch(typescriptSources, ['build']);
-    gulp.watch(jsSources,         ['browserify']);
+    gulp.watch(typescriptSources, ['browserify']);
     gulp.watch(htmlSources,       ['copyIndexHtml']);
 });
 
@@ -55,8 +38,6 @@ gulp.task('copyIndexHtml', function() {
 
 gulp.task('serve', ['default', 'watch'], serve('dist'));
 
-gulp.task('default', ['build', 'browserify', 'copyIndexHtml']);
+gulp.task('default', ['browserify', 'copyIndexHtml']);
 
-gulp.task('clean', function() {
-    del(jsSources.concat(['dist']));
-});
+gulp.task('clean', function() { del(['dist']); });
--- with-typescript/src/main.ts
+++ with-typescript/src/main.ts
@@ -1,41 +1,5 @@
-import React = require("react");
-import _     = require('underscore');
-
-var DOM = React.DOM;
-
-interface HelloState { counter: number }
-
-class Hello extends React.Component<any, HelloState> {
-
-    constructor (props) {
-        super(props, undefined);
-        this.state = { counter: 0 };
-    }
-
-    renderCounter = () => DOM.p({ key: 1 }, this.state.counter);
-
-    renderClicker = () =>
-        DOM.button(
-            { key: 2, onClick: evt => this.setState({ counter: this.state.counter + 1 })},
-            "Click me"
-        );
-
-    renderReset = () =>
-        DOM.button(
-            {key: 3, onClick: evt => this.setState({ counter: 0 })},
-            "Reset"
-        )
-
-    render() {
-        return DOM.p({}, [
-            _.map("now with typescript".split(" "), word => DOM.div({}, word.toUpperCase())),
-            this.renderCounter(),
-            this.renderClicker(),
-            this.renderReset(),
-        ]);
-
-    }
-}
+import React = require('react');
+import Hello = require('./hello');
 
 React.render(
     React.createElement(Hello, null),
