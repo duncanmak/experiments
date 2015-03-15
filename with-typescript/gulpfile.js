09e5bb0e4158e963ef621b29114676dd41494dd8
Duncan Mak <duncan@a-chinaman.com>
2015-03-14 23:34:13 -0400

    A better technique for dealing with relative paths in browserify

--- with-typescript/gulpfile.js
+++ with-typescript/gulpfile.js
@@ -4,31 +4,29 @@ var gulp       = require('gulp');
 var path       = require('path');
 var serve      = require('gulp-serve');
 var source     = require('vinyl-source-stream');
-
-var p = function(paths) {
-    var args = [].splice.call(arguments, 0);
-    return path.join.apply(undefined, [process.cwd()].concat(args));
-};
+var _          = require('underscore');
 
 var htmlSources = [
-    './index.html'
+    'index.html'
 ];
 
-var typescriptSources = [
-    p('src', 'hello.ts'),
-    p('src', 'main.ts')
-];
+var typescriptSources = relativeToRoot(
+    'src/main.ts',
+    'src/hello.ts'
+);
 
-var typings = [
-    p('typings', 'react', 'react.d.ts'),
-    p('typings', 'underscore', 'underscore.d.ts')
-];
+var typings = relativeToRoot(
+    'typings/react/react.d.ts',
+    'typings/underscore/underscore.d.ts'
+);
 
 gulp.task('browserify', function () {
-    browserify({ entries: typescriptSources.concat(typings), debug: true, fullPaths: false })
+    var entries = typescriptSources.concat(typings);
+
+    browserify({ entries: entries, debug: true })
         .plugin('tsify')
         .bundle()
-        .on('error', function(err){ console.log (err.message); this.end(); })
+        .on('error', function(err) { console.log (err.message); this.end(); })
         .pipe(source('bundle.js'))
         .pipe(gulp.dest('dist'));
 });
@@ -39,7 +37,7 @@ gulp.task('watch', function() {
 });
 
 gulp.task('copyIndexHtml', function() {
-    gulp.src('index.html').pipe(gulp.dest('dist'));
+    gulp.src(htmlSources).pipe(gulp.dest('dist'));
 });
 
 gulp.task('serve', ['default', 'watch'], serve('dist'));
@@ -47,3 +45,8 @@ gulp.task('serve', ['default', 'watch'], serve('dist'));
 gulp.task('default', ['browserify', 'copyIndexHtml']);
 
 gulp.task('clean', function() { del(['dist']); });
+
+function relativeToRoot () {
+    var splitPath = function(p) { return path.join.apply(undefined, p.split('/')); };
+    return _.map(arguments, _.compose(path.resolve, splitPath));
+};
